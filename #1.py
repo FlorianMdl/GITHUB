3fdc5cdf6418c0970851899a3e7401df777f6f9a
Florian Mdl <florian.mondanel@gmail.com>
2026-04-27 19:20:20 +0200

    Ajout des conditions sur l'âge utilisateur

--- #1.py
+++ #1.py
@@ -9,3 +9,10 @@ age = int(input("Age: "))
 sexe = str(input("Sexe: "))
 
 print(f"Enchanté {nom} {prenom}. Tu as {age} ans et tu es un {sexe}.")
+
+if age >= 18:
+    print("Tu es majeur. Accès autorisé.")
+elif 13 < age < 18:
+    print("Tu es ado. Accès restreint.")
+else: 
+    print("Tu es trop jeune. Accès refusé.")
