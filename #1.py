33cb88e11cb8bd392988343e163d5aecd0c0bd7b
Florian Mdl <florian.mondanel@gmail.com>
2026-04-27 19:06:23 +0200

    Ajout demande d'identité de l'utilisateur

---
+++ #1.py
@@ -0,0 +1,11 @@
+'''
+Recuperation des informations de bases sur l'utilisateur.
+'''
+
+
+nom = str(input("Nom: "))
+prenom = str(input("Prénom: "))
+age = int(input("Age: "))
+sexe = str(input("Sexe: "))
+
+print(f"Enchanté {nom} {prenom}. Tu as {age} ans et tu es un {sexe}.")
